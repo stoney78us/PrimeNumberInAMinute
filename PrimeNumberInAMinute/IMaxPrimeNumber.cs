f49262e01abfb518ed998fed06293753efec9e73
Vu Tran <vtran@quantumspatial.com>
2016-04-07 12:48:25 -0700

    Initial Commit. Creating program template.

---
+++ PrimeNumberInAMinute/IMaxPrimeNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberInAMinute
+{
+    public delegate void FoundPrimeCallBack(uint prime);
+
+    public interface IMaxPrimeNumber
+    {
+        //Find max prime number less than n. During finding max prime number, callback function will trigger when a new prime is found.
+        uint findMaxPrime(uint n, FoundPrimeCallBack callback);
+    }
+}
---
+++ PrimeNumberInAMinute/MaxPrimeNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberInAMinute
+{
+    public class MaxPrimeNumber : IMaxPrimeNumber
+    {
+        public uint findMaxPrime(uint n, FoundPrimeCallBack callback) {
+            return 0;
+        }
+    }
+}
